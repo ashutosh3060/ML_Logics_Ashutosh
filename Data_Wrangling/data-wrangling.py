3703eb92b3ceea5dd4894d71b38a9d27114e8664
Ashutosh <35258175+ashutosh3060@users.noreply.github.com>
2020-01-22 00:15:19 +0530

    Update data-wrangling.py

--- Data_Wrangling/data-wrangling.py
+++ Data_Wrangling/data-wrangling.py
@@ -2,3 +2,6 @@
 df.feature_name.loc[(df['feature_name'].isnull())] = 0
 df.feature_name.loc[(df['feature_name']=='Dinner')] = 1
 df.feature_name.loc[(df['feature_name']=='Lunch')] = 2
+
+# Exclude null, -infinity, infinity from a feature
+df_new = df.loc[~((df['feature_name']==np.inf)|(df['feature_name']==-(np.inf))|(df['feature_name'].isnull()))]
