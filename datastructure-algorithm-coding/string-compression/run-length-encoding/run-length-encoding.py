4e88b62ec7a4fc65f3059b266e565adfd1d1cc1b
Ashutosh <35258175+ashutosh3060@users.noreply.github.com>
2020-03-27 10:47:22 +0530

    Add files via upload

---
+++ datastructure-algorithm-coding/string-compression/run-length-encoding/run-length-encoding.py
@@ -0,0 +1,13 @@
+def run_len_encode_str_compr (string):
+    count = 1
+    result = []
+    for i in range(len(string)):
+        if i == len(string) - 1:
+            result.append([string[i], count])
+        elif string[i] == string[i+1]:
+            count += 1
+        else:
+            result.append([string[i], count])
+            count = 1
+    for i in result:
+        print(i[0]+str(i[1]), end="")
