3575cdaf29346ecbb4d0d626fb8573129109ce43
Ashutosh <35258175+ashutosh3060@users.noreply.github.com>
2020-04-04 09:38:10 +0530

    Update run-length-encoding.py

--- datastructure-algorithm-coding/string-compression/run-length-encoding/run-length-encoding.py
+++ datastructure-algorithm-coding/string-compression/run-length-encoding/run-length-encoding.py
@@ -1,6 +1,9 @@
+#Method 1:
+    
 def run_len_encode_str_compr (string):
     count = 1
     result = []
+    final_res = []
     for i in range(len(string)):
         if i == len(string) - 1:
             result.append([string[i], count])
@@ -10,4 +13,6 @@ def run_len_encode_str_compr (string):
             result.append([string[i], count])
             count = 1
     for i in result:
-        print(i[0]+str(i[1]), end="")
+        final_res.append(i[0]+str(i[1]))
+    final_res = ''.join(final_res)
+    return final_res
